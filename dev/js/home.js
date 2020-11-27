722f222052a2d1e873464e8810d267ce76bc2b48
ruslan bilokon <barslosu@gmail.com>
2020-11-28 00:23:02 +0200

    section servise&price

--- dev/js/home.js
+++ dev/js/home.js
@@ -1,41 +1,6 @@
 $(document).ready(function() {
     //TODO
 });
-$(function() {
-    var $menu_inner = $('.menu_inner');
-
-    $(".menu-triger").click(function() {
-        $('body').addClass('body_pointer');
-        $menu_inner.show(0);
-        $menu_inner.animate({ right: parseInt($menu_inner.css('left'), 10) == 0 ? -$menu_inner.outerWidth() : 0 },
-            300
-        );
-        return false;
-    });
-
-    $(".menu-close").click(function() {
-        $('body').removeClass('body_pointer');
-        $menu_inner.animate({ right: parseInt($menu_inner.css('right'), 10) == 0 ? -$menu_inner.outerWidth() : 0 },
-            300,
-            function() {
-                $menu_inner.hide(0);
-            }
-        );
-        return false;
-    });
-
-    $(document).on('click', function(e) {
-        if (!$(e.target).closest('.menu_inner').length) {
-            $('body').removeClass('body_pointer');
-            $menu_inner.animate({ right: parseInt($menu_inner.css('right'), 10) == 0 ? -$menu_inner.outerWidth() : 0 },
-                300,
-                function() {
-                    $menu_inner.hide(0);
-                }
-            );
-        }
-    });
-});
 
 window.onscroll = function() { myFunction() };
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,57 @@
 const
-    gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS     = require('gulp-clean-css'),
-    rename       = require('gulp-rename'),
-    htmlmin      = require('gulp-htmlmin'),
-    browserSync  = require('browser-sync').create(),
-    concat       = require('gulp-concat'),
-    uglify       = require('gulp-uglify'),
-    cache        = require('gulp-cache'),
-    imagemin     = require('gulp-imagemin'),
-    svgSprite    = require('gulp-svg-sprites'),
-    svgmin       = require('gulp-svgmin'),
-    cheerio      = require('gulp-cheerio'),
-    replace      = require('gulp-replace');
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cache = require('gulp-cache'),
+    imagemin = require('gulp-imagemin'),
+    svgSprite = require('gulp-svg-sprites'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace');
 
-gulp.task('browser-sync', ['fonts', 'styles', 'scripts', 'home-js', 'html', 'svgSprite', 'images'], function() {
+gulp.task('style', function() {
+    return gulp.src('./dev/scss/main.scss')
+        .pipe(sass({
+            includePaths: require('node-bourbon').includePaths
+        }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
+        .pipe(autoprefixer({ browsers: ['last 15 versions'], cascade: false }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
+});
+gulp.task('fonts', function() {
+    return gulp.src([
+            './dev/fonts/**/*'
+        ])
+        .pipe(gulp.dest('./app/fonts/'));
+});
+gulp.task('scripts', function() {
+    return gulp.src([
+            './dev/js/libs/jquery/jquery-3.4.1.js',
+            './dev/js/libs/slick/slick.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/js/'));
+});
+gulp.task('watch', function() {
+    gulp.watch('./dev/scss/**/*.scss', ['style']);
+    gulp.watch('./dev/*.html', ['html']);
+    gulp.watch('./dev/*.html').on('change', browserSync.reload);
+    gulp.watch('./dev/js/home.js', ['home-js']);
+    gulp.watch('./dev/js/*.js').on("change", browserSync.reload);
+    gulp.watch('./dev/js/libs/**/*.js', ['scripts']);
+    gulp.watch('./dev/img/svg/**/*.svg', ['svgSprite']);
+    gulp.watch('./dev/img/**/*.+(png|jpg|jpeg)', ['images']);
+});
+gulp.task('browser-sync', ['style', 'html', 'fonts', 'scripts', 'home-js', 'images', 'svgSprite'], function() {
     browserSync.init({
         server: {
             baseDir: "./app"
@@ -23,11 +59,25 @@ gulp.task('browser-sync', ['fonts', 'styles', 'scripts', 'home-js', 'html', 'svg
         notify: false
     });
 });
-
-//таск для преобразования всех свг иконок в один svg спрайт
-//
-
-gulp.task('svgSprite', function () {
+gulp.task('home-js', function() {
+    return gulp.src([
+            './dev/js/scripts/home.js'
+        ])
+        .pipe(concat('home.min.js'))
+        // .pipe(uglify())
+        .pipe(gulp.dest('./app/js/'));
+});
+gulp.task('html', function() {
+    return gulp.src([
+            './dev/*.html'
+        ])
+        // .pipe(htmlmin({
+        //     collapseWhitespace: true,
+        //     removeComments: true
+        // }))
+        .pipe(gulp.dest('./app'));
+});
+gulp.task('svgSprite', function() {
     return gulp.src('./dev/img/svg/**/*.svg')
         .pipe(svgmin({
             js2svg: {
@@ -35,107 +85,39 @@ gulp.task('svgSprite', function () {
             }
         }))
         .pipe(cheerio({
-          run: function ($) {
-            //удаляем из свг ненужные атрибуты
-            $('[fill]').removeAttr('fill');
-            $('[stroke]').removeAttr('stroke');
-            $('[style]').removeAttr('style');
-          },
-        //   parserOptions: {xmlMode: true}
+            run: function($) {
+                //удаляем из свг ненужные атрибуты
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            //   parserOptions: {xmlMode: true}
         }))
         .pipe(replace('&gt;', '>'))
+        // в поле selector добавляем уникальный префикс который далее будет использован в пути к свг спрайту
         .pipe(svgSprite({
-                mode: "symbols",
-                preview: false,
-                selector: "cli_%f",
-                svg: {
-                    symbols: 'sprite.svg'
-                }
+            mode: "symbols",
+            preview: false,
+            selector: "viva_%f",
+            svg: {
+                symbols: 'sprite.svg'
             }
-        ))
-        .pipe(gulp.dest('./app/img/svg/'))
-        .pipe(gulp.dest('./dest/img/svg/'));
+        }))
+        .pipe(gulp.dest('./app/img/svg/'));
 });
 
-gulp.task('styles', function () {
-    return gulp.src('./dev/scss/main.scss')
-        .pipe(sass({
-            includePaths: require('node-bourbon').includePaths
-        }).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix : ''}))
-        .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('app/css'))
-        .pipe(gulp.dest('dest/css'))
-        .pipe(browserSync.stream());
-});
-
-
-gulp.task('scripts', function() {
+gulp.task('images', function() {
     return gulp.src([
-        './dev/js/libs/jquery/jquery-3.4.1.js',
-        './dev/js/libs/slick/slick.min.js',
-    ])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./app/js/'))
-        .pipe(gulp.dest('./dest/js/'));
+            './dev/img/**/*.+(png|jpg|jpeg|svg)',
+            './dev/img/images/*.+(png|jpg|jpeg)',
+            '!/dev/img/svg/**/*.svg'
+        ])
+        // Caching images that ran through imagemin
+        // .pipe(cache(imagemin({
+        //     optimizationLevel: 5,
+        //     progressive: true,
+        //     interlaced: true
+        // })))
+        .pipe(gulp.dest('./app/img/'));
 });
-
-gulp.task('home-js', function() {
-    return gulp.src([
-        './dev/js/home.js'
-    ])
-        .pipe(concat('home.min.js'))
-        // .pipe(uglify())
-        .pipe(gulp.dest('./app/js/'))
-        .pipe(gulp.dest('./dest/js/'));
-});
-
-gulp.task('html', function() {
-    return gulp.src([
-        './dev/*.html'
-    ])
-        // .pipe(htmlmin({
-        //     collapseWhitespace: true,
-        //     removeComments: true
-        // }))
-        .pipe(gulp.dest('./app'));
-});
-
-gulp.task('images', function(){
-    return gulp.src([
-        './dev/img/**/*.+(png|jpg|jpeg|svg)',
-        './dev/img/images/*.+(png|jpg|jpeg)',
-        '!/dev/img/svg/**/*.svg'
-    ])
-    // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            optimizationLevel: 5,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('./app/img/'))
-        .pipe(gulp.dest('./dest/img/'));
-});
-
-gulp.task('fonts', function() {
-    return gulp.src([
-        './dev/fonts/**/*'
-    ])
-        .pipe(gulp.dest('./app/fonts/'))
-        .pipe(gulp.dest('./dest/fonts/'));
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./dev/scss/**/*.scss', ['styles']);
-    gulp.watch('./dev/js/libs/**/*.js', ['scripts']);
-    gulp.watch('./dev/js/home.js', ['home-js']);
-    gulp.watch('./dev/*.html', ['html']);
-    gulp.watch('./dev/img/svg/**/*.svg', ['svgSprite']);
-    gulp.watch('./dev/img/**/*.+(png|jpg|jpeg)', ['images']);
-    gulp.watch('./dev/js/*.js').on("change", browserSync.reload);
-    gulp.watch('./dev/*.html').on('change', browserSync.reload);
-});
-
 gulp.task('default', ['browser-sync', 'watch']);
