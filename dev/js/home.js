3244787fcf6e8d6165f29e7b58a4b0f86bfc8429
ruslan bilokon <barslosu@gmail.com>
2020-12-17 15:52:59 +0200

    new pages shop

--- dev/js/home.js
+++ dev/js/home.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    //TODO
+    $('.slider__product').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        focusOnSelect: true
+    });
 });
